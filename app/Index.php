af2c9d024f3555e580770b2f196d3e2052b276e8
SmoothjazzLounge <smoothjazzlounge87>
2017-07-17 15:47:13 +1000

    Перенёс таблицу в index.php

--- app/Index.php
+++ app/Index.php
@@ -2,3 +2,31 @@
 echo "TEST";
 
 ?>
+
+<!---->
+<table>
+    <thead>
+    <tr>
+        <th>1 Колонка</th>
+        <th>2 Колонка</th>
+        <th>3 Колонка</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>1 колонка</td>
+        <td>2 колонка</td>
+        <td>3 колонка</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    </tbody>
+</table>
